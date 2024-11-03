189a523e2d9e468367a746373d517750660b7152
anantariskys <gamersindo555@gmail.com>
2024-11-03 09:38:34 +0700

    Update Main.tsx

--- app/components/home/Main.tsx
+++ app/components/home/Main.tsx
@@ -1,22 +1,60 @@
-import React from "react";
+import { Form, useActionData } from "@remix-run/react";
+import Button from "../Button";
 
-type User = {
-  id: string;
+interface Post {
+  id: number;
+  content: string;
+  userId: number;
+  user: User;
+}
+interface User {
+  id: number;
   email: string;
   username: string;
-};
+  password: string;
+}
 
-type MainProps = {
-  user: User;
-};
+interface Props {
+  posts: Post[];
+}
 
-const Main: React.FC<MainProps> = ({ user }) => {
+const Main: React.FC<Props> = ({ posts }) => {
+  const actionData = useActionData<{ error: string }>();
+
   return (
-    <main>
-      <h1>Dashboard</h1>
-      <p>Welcome, {user.username}!</p>
-      <p>User ID: {user.id}</p>
-      <p>Email: {user.email}</p>
+    <main className="py-4 space-y-2 w-full">
+      <input
+        type="search"
+        placeholder="search"
+        className="border-2 border-primary rounded-lg px-2 py-1"
+      />
+
+      <Form className="flex gap-2" method="post">
+        <input type="hidden" name="actionType" value="post" />
+        <div className="flex flex-col">
+          <input
+            type="text"
+            name="content"
+            placeholder={`What's on your mind?`}
+            className="border-2 border-primary rounded-lg px-2 py-1"
+          />
+          {actionData?.error && (
+            <small className="text-red-500">{actionData.error}</small>
+          )}
+        </div>
+        <Button type="submit" variant="default" width="w-fit">
+          Post!
+        </Button>
+      </Form>
+
+      <section className=" py-4 w-full max-w-2xl ">
+        {posts.map((post) => (
+          <div key={post.id} className=" border-primary  px-2 py-1">
+            <small className="font-semibold">{post.user.username}</small>
+            <p>{post.content}</p>
+          </div>
+        ))}
+      </section>
     </main>
   );
 };
