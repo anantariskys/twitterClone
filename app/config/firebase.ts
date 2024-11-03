035be556298caf445dd2872108b4cd51ab2553d1
anantariskys <gamersindo555@gmail.com>
2024-11-04 00:33:48 +0700

    Create firebase.ts

---
+++ app/config/firebase.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBLkxzcsv-grj9UxTiKJ2rp8fdoPG39vfc",
+    authDomain: "n-blog-next.firebaseapp.com",
+    projectId: "n-blog-next",
+    storageBucket: "n-blog-next.appspot.com",
+    messagingSenderId: "800664885227",
+    appId: "1:800664885227:web:e9c61f6f6aee693e89abae",
+    measurementId: "G-5KFPC3L7CQ"
+  };
+
+  export default firebaseConfig
