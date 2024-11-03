38f7143636a49864bb6ad3ffd9281e8188ae56cc
anantariskys <gamersindo555@gmail.com>
2024-11-03 09:38:32 +0700

    Create getAllPost.ts

---
+++ app/actions/post/getAllPost.ts
@@ -0,0 +1,13 @@
+import { json } from "@remix-run/node";
+import {prisma } from "~/db.server";
+
+const getAllPost = async () => {
+    const posts = await prisma.post.findMany({
+        include:{
+            user:true
+        }
+    });
+    return posts;
+};
+
+export default getAllPost
