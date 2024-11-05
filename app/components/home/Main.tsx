0ac0c3ded54d7f5cb2945a10d0c1cf1b0f3c784e
anantariskys <gamersindo555@gmail.com>
2024-11-05 12:10:05 +0700

    Update Main.tsx

--- app/components/home/Main.tsx
+++ app/components/home/Main.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Form, useActionData } from "@remix-run/react";
 import Button from "../Button";
 import Toast from "../Toast";
+import TweetCard from "../TweetCard";
+import PostForm from "./PostForm";
 
 interface Post {
   id: number;
@@ -9,12 +11,7 @@ interface Post {
   userId: number;
   user: User;
   created_at: string;
-  image: Image[];
-}
-
-interface Image {
-  id: number;
-  url: string;
+  image: string;
 }
 
 interface User {
@@ -29,76 +26,21 @@ interface Props {
 }
 
 const Main: React.FC<Props> = ({ posts }) => {
-  const actionData = useActionData<{ error: string }>();
-  console.log("Action Data:", actionData);
+  const actionData = useActionData<{ error?: string; message?: string }>();
   const [showToast, setShowToast] = useState(false);
 
   useEffect(() => {
-    console.log("Effect running. Action Data:", actionData);
-    setShowToast(!!actionData?.error);
+    setShowToast(!!actionData?.error || !!actionData?.message);
   }, [actionData]);
 
-  console.log("Render Main Component");
-
   return (
-    <main className="p-4 space-y-4 w-full">
-      <input
-        type="search"
-        placeholder="search"
-        className="border-2 border-primary rounded-lg px-2 py-1"
-      />
-
-      <Form className="flex flex-col gap-4" method="post">
-        <input type="hidden" name="actionType" value="post" />
-
-        <div className="flex flex-col">
-          <input
-            type="text"
-            name="content"
-            placeholder={`What's on your mind?`}
-            className="border-2 border-primary rounded-lg px-2 py-1"
-          />
-        </div>
-        {/* 
-        <div className="flex flex-col">
-          <label htmlFor="images" className="text-sm font-semibold">
-            Upload Images
-          </label>
-          <input
-            type="file"
-            name="images"
-            id="images"
-            multiple
-            accept="image/*"
-            onChange={handleImageChange}
-            className="border-2 border-primary rounded-lg px-2 py-1"
-          />
-          {imagePreviews.length > 0 && (
-            <div className="flex gap-2 mt-2">
-              {imagePreviews.map((src, index) => (
-                <div key={index} className="relative w-20 h-20">
-                  <img src={src} alt={`Preview ${index}`} className="w-full h-full object-cover rounded-md" />
-                </div>
-              ))}
-              <button
-                type="button"
-                onClick={handleClearImages}
-                className="text-red-500 font-semibold"
-              >
-                Clear Images
-              </button>
-            </div>
-          )}
-        </div> */}
-
-        <Button type="submit" variant="default" width="w-fit">
-          Post!
-        </Button>
-      </Form>
-
+    <main className=" w-full">
+      <PostForm />
       {showToast && (
         <Toast
-          message={actionData?.error || "Something went wrong"}
+          message={
+            actionData?.error || actionData?.message || "Something went wrong"
+          }
           type="primary"
           position={true}
           classname="bottom-4 right-4"
@@ -106,25 +48,9 @@ const Main: React.FC<Props> = ({ posts }) => {
           duration={2000}
         />
       )}
-
-      <section className="py-4 w-full max-w-2xl space-y-2">
+      <section className="p-4 w-full max-w-2xl space-y-2">
         {posts.map((post) => (
-          <div key={post.id} className="border shadow-lg rounded-md px-2 py-1">
-            {/* {post.image?.length > 0 && (
-              <div className="flex gap-2">
-                {post.image.map((img) => (
-                  <div key={img.id} className="relative w-20 h-20">
-                    <img src={img.url} alt={`Post image`} className="w-full h-full object-cover rounded-md" />
-                  </div>
-                ))}
-              </div>
-            )} */}
-            <div className="flex justify-between">
-              <small className="font-semibold">{post.user.username}</small>
-              <small>{new Date(post.created_at).toLocaleString()}</small>
-            </div>
-            <p>{post.content}</p>
-          </div>
+          <TweetCard post={post} key={post.id} />
         ))}
       </section>
     </main>
