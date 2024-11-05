0ba24e58212caaf3c9726ef22c2024511087402b
anantariskys <gamersindo555@gmail.com>
2024-11-05 12:09:59 +0700

    Update createPost.ts

--- app/actions/post/createPost.ts
+++ app/actions/post/createPost.ts
@@ -1,19 +1,34 @@
-
-import { json } from "@remix-run/node";
+import { data, json } from "@remix-run/node";
 import { prisma } from "~/db.server";
 import { uploadImageToFirebase } from "~/utils/firebase";
 
 const createPost = async (formData: FormData, userId: number) => {
   const content = formData.get("content")?.toString() || "";
-  const imageFile = formData.get("images") as File;
+  const imageFile = formData.get("image") as File;
+
+ 
 
   if (!content) {
     return json({ error: "All fields are required" }, { status: 400 });
   }
 
+  if (imageFile.name) {
+    
+    const url = await uploadImageToFirebase(imageFile);
+    const post = await prisma.post.create({
+      data: {
+        content,
+        userId,
+        image: url,
+      },
+    });
 
+    return json(
+      { message: "Post created successfully with image", data: post },
+      { status: 200 }
+    );
+  }
 
-  
   const post = await prisma.post.create({
     data: {
       content,
@@ -21,7 +36,10 @@ const createPost = async (formData: FormData, userId: number) => {
     },
   });
 
-  return json({ message: "Post created successfully"}, { status: 200 });
+  return json(
+    { message: "Post created successfully without image"},
+    { status: 200 }
+  );
 };
 
 export default createPost;
